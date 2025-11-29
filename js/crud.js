670801d6194ba78efde0a5829e57362393e38939
RoaTheDev <roachamreun15@proton.me>
2025-11-29 15:00:58 +0700

    partially add crud utils

---
+++ js/crud.js
@@ -0,0 +1,28 @@
+import {localStorageUtils} from "./localStorageUtils";
+
+const userLocalStorageKey = "userList";
+export const startUp = async () => {
+    const data = localStorageUtils.get(userLocalStorageKey)
+    if (data) {
+        return data.users;
+    }
+    const newData = await fetchMockUsers();
+    localStorage.setItem(userLocalStorageKey, newData.users);
+    return newData;
+}
+
+export const getUserById = (id) => {
+    const data = localStorageUtils.get(userLocalStorageKey);
+
+    if (!data || !Array.isArray(data.users)) return null;
+
+    return data.users.find((user) => user.id === id) || null;
+};
+
+export const deleteUserById = (id, users) => {
+    if (!users || !Array.isArray(users)) return null;
+    const newArray = [...users.filter(item => item.id !== id)];
+    localStorageUtils.set(userLocalStorageKey, newArray);
+    return newArray;
+}
+
